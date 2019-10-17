d0c0501de60ea6e322614c6a4c48c2f343d30e49
zhangpengshou <zhangpengshou@hotmail.com>
2019-10-17 14:19:46 +0800

    可用版本

---
+++ src/main/java/pencil/oauth2service/Oauth2ServiceApplication.java
@@ -0,0 +1,12 @@
+package pencil.oauth2service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2ServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(Oauth2ServiceApplication.class, args);
+    }
+
+}
